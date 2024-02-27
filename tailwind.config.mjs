9498700d86303949327a57186d9754ca7790f9c5
Liam Marega <86364396+LiamMarega@users.noreply.github.com>
2024-02-27 17:16:12 -0300

    Actualizar estilos de texto y agregar animación de texto en AboutMe3.astro y tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
