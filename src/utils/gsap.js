a43ccc4b1fe0ebf664715efb334085047aeff625
Liam Marega <86364396+LiamMarega@users.noreply.github.com>
2024-03-03 15:48:42 -0300

    Actualiza estilos y animaciones en componentes de trabajo

--- src/utils/gsap.js
+++ src/utils/gsap.js
@@ -4,20 +4,42 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 const tl = gsap.timeline();
+const tlWork = gsap.timeline();
 let sections = gsap.utils.toArray('.section');
-
-tl.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  duration: 2,
-  ease: 'none',
-});
 
 ScrollTrigger.create({
   animation: tl,
-  markers: true,
   trigger: '#about',
   start: '5% top',
   pin: true,
   scrub: 0.5,
   end: () => '+=' + document.querySelector('#about').offsetWidth - window.innerWidth,
+});
+
+ScrollTrigger.create({
+  animation: tlWork,
+  trigger: '#work',
+  start: 'top top',
+  pin: true,
+  scrub: 0.5,
+  snap: 1 / (sections.length - 1),
+  end: () => '+=' + document.querySelector('#work').offsetHeight,
+});
+
+tl.to(sections, {
+  xPercent: -200,
+  duration: 2,
+  ease: 'none',
+});
+
+tlWork.to('#advantisAI', {
+  yPercent: -100,
+  duration: 2,
+  ease: 'none',
+});
+
+tlWork.to('#fmoney', {
+  yPercent: -200,
+  duration: 2,
+  ease: 'none',
 });
