43fb859acc66a937d3327b6e80e56549fbbdf5b1
Liam Marega <86364396+LiamMarega@users.noreply.github.com>
2024-03-04 12:05:20 -0300

    Actualizar estilos y agregar nuevo trabajo

--- src/utils/gsap.js
+++ src/utils/gsap.js
@@ -7,39 +7,46 @@ const tl = gsap.timeline();
 const tlWork = gsap.timeline();
 let sections = gsap.utils.toArray('.section');
 
-ScrollTrigger.create({
-  animation: tl,
-  trigger: '#about',
-  start: '5% top',
-  pin: true,
-  scrub: 0.5,
-  end: () => '+=' + document.querySelector('#about').offsetWidth - window.innerWidth,
-});
+if (window.innerWidth > 859) {
+  ScrollTrigger.create({
+    animation: tl,
+    trigger: '#about',
+    start: '5% top',
+    pin: true,
+    scrub: 0.5,
+    end: () => '+=' + document.querySelector('#about').offsetWidth - window.innerWidth,
+  });
 
-ScrollTrigger.create({
-  animation: tlWork,
-  trigger: '#work',
-  start: 'top top',
-  pin: true,
-  scrub: 0.5,
-  snap: 1 / (sections.length - 1),
-  end: () => '+=' + document.querySelector('#work').offsetHeight,
-});
+  ScrollTrigger.create({
+    animation: tlWork,
+    trigger: '#work',
+    start: 'top top',
+    pin: true,
+    scrub: 0.5,
+    end: '+=3000',
+  });
 
-tl.to(sections, {
-  xPercent: -200,
-  duration: 2,
-  ease: 'none',
-});
+  tl.to(sections, {
+    xPercent: -200,
+    duration: 2,
+    ease: 'none',
+  });
 
-tlWork.to('#advantisAI', {
-  yPercent: -100,
-  duration: 2,
-  ease: 'none',
-});
+  tlWork.to('#advantisAI', {
+    yPercent: -100,
+    duration: 2,
+    ease: 'none',
+  });
 
-tlWork.to('#fmoney', {
-  yPercent: -200,
-  duration: 2,
-  ease: 'none',
-});
+  tlWork.to('#fmoney', {
+    yPercent: -200,
+    duration: 2,
+    ease: 'none',
+  });
+
+  tlWork.to('#lamshoes', {
+    yPercent: -300,
+    duration: 2,
+    ease: 'none',
+  });
+}
