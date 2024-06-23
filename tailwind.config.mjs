abbc262c65dc1cdad3132350d81cb60bbf3f3889
Liam Marega <86364396+LiamMarega@users.noreply.github.com>
2024-06-23 12:43:46 -0300

    fix responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    screens: {
+      mini: { raw: '(min-height: 800px)' },
+      // => @media (min-height: 800px) { ... }
+    },
     extend: {
       animation: {
         text: 'text 5s ease infinite',
